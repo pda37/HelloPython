b6b54bc9584dfd1e75680bc8deb8e9d8ca5c4f44
pda37 <1021665619@qq.com>
2022-03-10 16:10:10 +0800

    检测大写字母

--- basic_python/python_study01.py
+++ basic_python/python_study01.py
@@ -1860,3 +1860,13 @@
 #
 #
 # print(findMaxConsecutiveOnes([1,1,0,1,1,1]))
+
+# def detectCapitalUse(word):
+#     if word.upper() == word or word.lower() == word:
+#         return True
+#     elif word[0].upper() == word[0] and word[1:].lower() == word[1:]:
+#         return True
+#     return False
+#
+#
+# print(detectCapitalUse('Wer'))
