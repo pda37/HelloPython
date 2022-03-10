a8b9c124e8a30116fcf2eddfe266ad8908400c83
pda37 <1021665619@qq.com>
2022-03-10 17:48:02 +0800

    学生出勤记录 I + 反转字符串 II

--- basic_python/python_study01.py
+++ basic_python/python_study01.py
@@ -1870,3 +1870,38 @@
 #
 #
 # print(detectCapitalUse('Wer'))
+
+# 学生出勤记录 I
+# def checkRecord(s: str):
+#     if s.count('A') > 1 or s.count('LLL') > 0:
+#         return False
+#     return True
+#
+#
+# print(checkRecord('PAPLPLL'))
+
+# 反转字符串 II
+# def reverseStr(s: str, k: int):
+#     if len(s) < k:
+#         return s[::-1]
+#     else:
+#         n = int(len(s)/k)
+#         ans = []
+#         for i in range(n):
+#             if i % 2 == 0:
+#                 if i == 0:
+#                     ans.append(s[i * k + k - 1::-1])
+#                 else:
+#                     ans.append(s[i * k + k-1:i*k-1:-1])
+#             else:
+#                 ans.append(s[i*k:i*k+k])
+#         if len(s) % k != 0:
+#             if n % 2 == 0:
+#                 ans.append(s[:n * k - 1:-1])
+#             else:
+#                 ans.append(s[n * k:])
+#         a = ''.join(ans)
+#         return a
+#
+#
+# print(reverseStr('ab', 3))
